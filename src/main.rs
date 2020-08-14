18b0cdee5676378b205dcb6b4b6d32b0dcdd7065
Jordan Grace <jordan.t.grace@gmail.com>
2020-08-14 10:33:29 -0400

    Removed wgpu tutorial code

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,32 @@
-// pub mod app;
-// pub mod blob;
-// pub mod config;
-// pub mod ecs;
-// pub mod math;
-// pub mod world;
-pub mod wgpu_tutorial;
+pub mod app;
+pub mod blob;
+pub mod config;
+pub mod ecs;
+pub mod math;
+pub mod world;
+use app::App;
+use config::Config;
+use world::World;
 
-// use app::App;
-// use config::Config;
-// use world::World;
+const WIDTH: u32 = 500;
+const HEIGHT: u32 = 500;
 
-// const WIDTH: u32 = 500;
-// const HEIGHT: u32 = 500;
-
-// const BLOB_COUNT: usize = 10;
+const BLOB_COUNT: usize = 10;
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    wgpu_tutorial::main()
+    let config = Config::load_default_config_file()?;
 
-    // let config = Config::load_default_config_file()?;
+    let mut world = World {
+        width: WIDTH as usize,
+        height: HEIGHT as usize,
+        config,
+        blobs: Vec::new(),
+    };
+    world.add_random_blobs(BLOB_COUNT);
 
-    // let mut world = World {
-    //     width: WIDTH as usize,
-    //     height: HEIGHT as usize,
-    //     config,
-    //     blobs: Vec::new(),
-    // };
-    // world.add_random_blobs(BLOB_COUNT);
+    let app = App::new(world);
 
-    // let app = App::new(world);
-
-    // app.run()
+    app.run()
 }
